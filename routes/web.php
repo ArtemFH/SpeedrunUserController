89663c53b314710c2f2930020bb0ea9c6061cf94
ArtemFH <artembereza1@mail.ru>
2021-11-11 04:25:32 +0700

    Update router

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TopicController;
+use App\Http\Controllers\ReplyController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+})->name('home');
+
+Route::name('user.')->group(function () {
+    Route::get('/registration', [UserController::class, 'registrationAvailability'])->name('registration');
+
+    Route::post('/registration', [UserController::class, 'registration']);
+
+    Route::get('/login', [UserController::class, 'loginAvailability'])->name('login');
+
+    Route::post('/login', [UserController::class, 'login']);
+
+    Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 });
 
-Route::resource('test', \App\Http\Controllers\TestController::class);
+Route::resource('user', UserController::class);
+Route::resource('topic', TopicController::class);
+Route::resource('reply', ReplyController::class);
