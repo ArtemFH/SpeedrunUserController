1b9b3a378adb8afd1bb78605796277185ee1560c
ArtemFH <artembereza1@mail.ru>
2021-11-13 10:43:13 +0700

    Add resource and specificity URL GET|POST

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\TopicController;
 use App\Http\Controllers\ReplyController;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/', [TopicController::class, 'index'])->name('home');
 
 Route::name('user.')->group(function () {
     Route::get('/registration', [UserController::class, 'registrationAvailability'])->name('registration');
@@ -20,7 +18,7 @@ Route::name('user.')->group(function () {
 
     Route::get('/logout', [UserController::class, 'logout'])->name('logout');
 });
-
+Route::post('/send/{id}', [ReplyController::class, 'store'])->name('send');
 Route::resource('user', UserController::class);
 Route::resource('topic', TopicController::class);
 Route::resource('reply', ReplyController::class);
